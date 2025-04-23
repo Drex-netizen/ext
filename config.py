b4de1498b7fa6371db9526711c3d976bda30a4a7
Drex-netizen <divyeshkalleda09@gmail.com>
2025-04-23 13:27:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,27 +2,27 @@ import os
 
 class Config(object):
     # Telegram Bot ka token
-    BOT_TOKEN = "7876315583:AAEfPrw1IqQCLF2bdRw8-9LCiM_mRkrYQQo"
+    BOT_TOKEN = "8070832570:AAFEFf_0elawe_xKUHAwxNtAiIPG-DIAlAs"
     # Telegram API ki ID
-    API_ID = 27886943
-    API_HASH = "db28d0dae5a57f767716f1d8865560fee"
-    ADMIN_ID = [6586191138]
+    API_ID = 28649336
+    API_HASH = "3239c74cbd57eb043a9c74e06be3028b"
+    ADMIN_ID = [8117799250]
     # MongoDB database ka URL
     DB_URL = "mongodb+srv://ravinderkalleda15:s2LCPiwJTJFS0r8f@databse1.es6ih9o.mongodb.net/?retryWrites=true&w=majority&appName=Databse1"
     # Database ka naam
-    DB_NAME = "MY_BOT_DB"
+    DB_NAME = "Databse1"
     # Text log channel ki ID
-    TXT_LOG = -1002632380137
+    TXT_LOG = -1002630635671
     # Authentication log channel ki ID
-    AUTH_LOG = -1002528460802
+    AUTH_LOG = -1002452279727
     # Hit log channel ki ID
-    HIT_LOG = -1002604109457
+    HIT_LOG = -1002601203179
     # DRM dump channel ki ID
-    DRM_DUMP = -1002628576734
+    DRM_DUMP = -1002438294791
     # Main channel ki ID
-    CHANNEL = -1002251651410
+    CHANNEL = -1002631990606
     # Channel ka link
-    CH_URL = "https://t.me/+qEEho66dZJ1kODRi"
+    CH_URL = "https://t.me/+b_pC656ImYJiZmE9"
     # Bot ke owner ka Telegram link
     OWNER = "https://t.me/username"
     # Thumbnail image ka URL
